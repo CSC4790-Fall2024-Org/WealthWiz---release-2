6da3105906dc48dc50dadffbc6a5f899419f2c23
jfantasia28 <jfantasia28@gmail.com>
2024-10-30 11:31:37 -0400

    Fixed Navigation for Course 1 For Now
    Slight text formatting changes

--- WealthWizApp/screens/C1/CourseHome.js
+++ WealthWizApp/screens/C1/CourseHome.js
@@ -18,21 +18,30 @@ import {
 } from "../../GlobalStyles";
 import NavBar1 from "../../components/NavBar1";
 import { useNavigation } from "@react-navigation/native";
+import Button from "../../components/Button";
 
 const CourseHome = () => {
   const navigation = useNavigation();
   return (
-    <View style={styles.coursehome}>
+    <View style={styles.quiz1}>
       <NavBar1/>
       <Text style={styles.text}>
-        Course Home
+        The Stock Market
       </Text>
+      <Button
+          title="Start Learning" 
+          onPress={() => navigation.navigate("M1")}
+          buttonColor={Color.colorSeagreen} 
+          textColor={Color.black0} 
+          height={65}
+          width={350}
+      />
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-  coursehome: {
+    quiz1: {
     backgroundColor: Color.black0,
     flex: 1,
     alignItems: 'center',
@@ -44,6 +53,10 @@ const styles = StyleSheet.create({
     color: Color.colorDarkslategray_200,
     fontFamily: 'lexend-regular',
     paddingBottom: 20,
+    paddingTop:40,
+    marginLeft: 40,
+    marginRight: 40
+    
   },
 });
 
